013f112ccdaea30e8db2ba1e96ae5a4532dedbf3
Ian Andrewson <ianandrewson@gmail.com>
2019-12-05 16:15:19 -0800

    test for PersonModel written. Ready to TDD

---
+++ models/PersonModel.test.js
@@ -0,0 +1,26 @@
+const Person = require('./PersonModel');
+
+describe('PersonModel tests', () => {
+  it('should have a moral compass', () => {
+    const person = new Person({
+    });
+    const { errors } = person.validateSync();
+    expect(errors.moralCompass.message).toEqual('Path `moralCompass` is required.');
+  });
+  it('should have a soul', () => {
+    const person = new Person({
+      moralCompass: true
+    });
+    const { errors } = person.validateSync();
+    expect(errors.soul.message).toEqual('Path `soul` is a required field.')
+  });
+  it('should have one stomach', () => {
+    const person = new Person({
+      moralCompass: true,
+      soul: true,
+      stomachs: 2
+    });
+    const { errors } = person.validateSync();
+    expect(errors.stomachs.message).toEqual('Path `stomachs` (2) is greater than maximum allowed value.');
+  });
+});
